65ac08ecef3f5e510406710ab295b03aa08b6fad
Zeeshan Ahmed <zeeshan.ahmed@invozone.com>
2021-09-15 14:02:32 +0500

    Extension for HKUpdateFrequency added to avoid string typos

--- OTFMagicBox/Library/OTFHealthKitManager.swift
+++ OTFMagicBox/Library/OTFHealthKitManager.swift
@@ -12,7 +12,7 @@ class OTFHealthKitManager : NSObject {
     
     fileprivate var hkTypesToReadInBackground: Set<HKQuantityType> = []
     
-    override init() {
+    private override init() {
         let healthKitData = YmlReader().healthKitDataToRead as Array
         for requestedHKType in healthKitData {
             let id = HKQuantityTypeIdentifier(rawValue: "HKQuantityTypeIdentifier" + requestedHKType.type)
@@ -24,30 +24,47 @@ class OTFHealthKitManager : NSObject {
     /// Query for HealthKit Authorization
     /// - Parameter completion: (success, error)
     func getHealthAuthorization(_ completion: @escaping (_ success: Bool, _ error: Error?) -> Void) {
-     /* **************************************************************
-        * customize HealthKit data that will be collected
-        * in the background. Choose from any HKQuantityType:
-        * https://developer.apple.com/documentation/healthkit/hkquantitytypeidentifier
-       **************************************************************/
+        /* **************************************************************
+         * customize HealthKit data that will be collected
+         * in the background. Choose from any HKQuantityType:
+         * https://developer.apple.com/documentation/healthkit/hkquantitytypeidentifier
+         * *************************************************************/
        
-       // handle authorization from the OS
-       ActivityManager.shared.getHealthAuthorizaton(forTypes: hkTypesToReadInBackground) { [weak self] (success, error) in
-           if (success) {
-            let frequency = YmlReader().backgroundReadFrequency
+        // handle authorization from the OS
+        ActivityManager.shared.getHealthAuthorizaton(forTypes: hkTypesToReadInBackground) { (success, error) in
+            if (success) {
+                let frequency = YmlReader().backgroundReadFrequency
 
-               if frequency == "daily" {
-                   ActivityManager.shared.startHealthKitCollectionInBackground(withFrequency: .daily)
-               } else if frequency == "weekly" {
-                   ActivityManager.shared.startHealthKitCollectionInBackground(withFrequency: .weekly)
-               } else if frequency == "hourly" {
-                   ActivityManager.shared.startHealthKitCollectionInBackground(withFrequency: .hourly)
-               } else {
-                   ActivityManager.shared.startHealthKitCollectionInBackground(withFrequency: .immediate)
-               }
-           }
-           completion(success, error)
-       }
-   }
-    
+                if frequency == HKUpdateFrequency.daily.stringValue {
+                    ActivityManager.shared.startHealthKitCollectionInBackground(withFrequency: .daily)
+                } else if frequency == HKUpdateFrequency.weekly.stringValue {
+                    ActivityManager.shared.startHealthKitCollectionInBackground(withFrequency: .weekly)
+                } else if frequency == HKUpdateFrequency.hourly.stringValue {
+                    ActivityManager.shared.startHealthKitCollectionInBackground(withFrequency: .hourly)
+                } else {
+                    ActivityManager.shared.startHealthKitCollectionInBackground(withFrequency: .immediate)
+                }
+            }
+            completion(success, error)
+        }
+    }
 }
 
+
+extension HKUpdateFrequency {
+    var stringValue: String {
+        switch self {
+        case .hourly:
+            return "hourly"
+        
+        case .daily:
+            return "daily"
+        
+        case .weekly:
+            return "weekly"
+        
+        default:
+            return "immediate"
+        }
+    }
+}
