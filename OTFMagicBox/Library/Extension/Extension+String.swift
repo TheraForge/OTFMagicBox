6f0f40162d38185f51d0f8f00e97c7abc9096991
SpurtiB <spurti.ranjanagi@gmail.com>
2021-10-14 21:50:39 +0200

    Updated the Yaml hex Colors with the Color palette

--- OTFMagicBox/Library/Extension/Extension+String.swift
+++ OTFMagicBox/Library/Extension/Extension+String.swift
@@ -18,12 +18,56 @@ extension String {
         if count == 0 {
             return false
         }
-        for character in self {
-            if character.isEmoji {
+        for character in self where character.isEmoji {
                 return true
-            }
         }
         return false
+    }
+    
+    // swiftlint:disable all
+    func color() -> UIColor? {
+        switch self {
+        case "Red":
+                return UIColor.red
+        case "Green":
+                return UIColor.green
+        case "Indigo":
+                return UIColor.systemIndigo
+        case "Purple":
+                return UIColor.purple
+        case "Blue":
+                return UIColor.blue
+        case "Brown":
+                return UIColor.brown
+        case "Orange":
+                return UIColor.orange
+        case "Cyan":
+                return UIColor.cyan
+        case "Teal":
+                return UIColor.systemTeal
+        case "Yellow":
+                return UIColor.yellow
+        case "Pink":
+                return UIColor.systemPink
+        case "Gray":
+                return UIColor.gray
+        case "Gray2":
+                return UIColor.systemGray2
+        case "Gray3":
+                return UIColor.systemGray3
+        case "Gray4":
+                return UIColor.systemGray4
+        case "Gray5":
+                return UIColor.systemGray5
+        case "Gray6":
+                return UIColor.systemGray6
+        case "Black":
+                return UIColor.black
+        case "White":
+                return UIColor.white
+        default:
+                return nil
+        }
     }
 
 }
