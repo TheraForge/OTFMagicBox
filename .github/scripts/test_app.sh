bef60429f0e71d9703793e576fc5bb3678009504
Zeeshan Ahmed <zeeshan.ahmed@invozone.com>
2022-07-17 23:00:25 +0500

    build command modified

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -4,5 +4,4 @@ set -eo pipefail
 
 xcodebuild -workspace OTFMagicBox.xcworkspace \
             -scheme OTFMagicBox \
-            -destination platform=iOS\ Simulator,OS=15.5,name=iPhone\ 13 \
             clean test | xcpretty
