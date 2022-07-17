53ce81cf6c0bc3aed14984aa7ef9de225d8617d4
Zeeshan Ahmed <zeeshan.ahmed@invozone.com>
2022-07-17 23:19:31 +0500

    github credentials step added to deploy workflow, repo username moved to secrets, and destination added to build command

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -4,4 +4,5 @@ set -eo pipefail
 
 xcodebuild -workspace OTFMagicBox.xcworkspace \
             -scheme OTFMagicBox \
+            -destination platform=iOS\ Simulator,OS=15.2,name=iPhone\ 13 \
             clean test | xcpretty
